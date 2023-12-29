a0fcca8a4e8df75b3769d61590db2ffb3426e97d
Pascal Geiser <10461883+13pgeiser@users.noreply.github.com>
2023-12-29 09:54:58 +0100

    Clean before and after building

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ for step in $steps; do
 			;;
 		"apt")
 			sudo apt install -y sed make binutils gcc g++ bash build-essential \
-				patch gzip bzip2 perl tar cpio python2 python3 unzip rsync wget \
+				patch gzip bzip2 perl tar cpio python3 unzip rsync wget \
 				libncurses-dev bc findutils zstd
 			;;
 		"defconfig")
